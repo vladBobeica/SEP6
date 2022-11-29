b5e2661e98df7c0831a34a9fdc8da6b75566347e
Vlad Bobeica <vlad.bobeica@outlook.com>
2022-11-29 20:28:10 +0100

    Add icons, add header

---
+++ components/Header.tsx
@@ -0,0 +1,35 @@
+import { BellIcon, SearchIcon } from "@heroicons/react/solid"
+import Link from "next/link"
+
+function Header() {
+    return <header>
+        <div className="flex items-center space-x-2 md:space-x-10">
+            <img
+                src="https://i.postimg.cc/6p42F4np/Netflix-2015-logo.png"
+                width={100}
+                height={100}
+                className="cursor-pointer object-contain"
+            />
+
+            <ul className="hidden space-x-4 md:flex">
+                <li className="headerLink">Home</li>
+                <li className="headerLink">TV Shows</li>
+                <li className="headerLink">Movies</li>
+                <li className="headerLink">New & Popular</li>
+                <li className="headerLink">My list</li>
+            </ul>
+        </div>
+
+        <div className="flex items-center space-x-4 text-sm font-light">
+            <SearchIcon className="hidden h-6 w-6 sm:inline" />
+            <p className="hidden lg:inline">Kids</p>
+            <BellIcon className="h-6 w-6" />
+            <Link href="/account">
+                <img src="https://rb.gy/g1pwyx" alt="" className="cursor-pointer rounded" />
+            </Link>
+        </div>
+
+    </header>
+}
+
+export default Header
