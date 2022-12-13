759233484864f60ee73f8abe2179fd1202bfb66f
Vlad Bobeica <vlad.bobeica@outlook.com>
2022-12-13 17:31:17 +0100

    .gitignore is now working add icon logout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,63 +1,53 @@
-import { BellIcon, SearchIcon } from "@heroicons/react/solid"
-import Link from "next/link"
-import { useEffect, useState } from "react"
-import useAuth from "../hooks/useAuth"
+import { BellIcon, SearchIcon } from "@heroicons/react/solid";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import useAuth from "../hooks/useAuth";
 
 function Header() {
-    const[isScrolled, setIsScrolled] = useState(false)
-    const {logout} = useAuth()
+  const [isScrolled, setIsScrolled] = useState(false);
+  const { logout } = useAuth();
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
 
-    useEffect(() => {
-        const handleScroll = () => {
-          if (window.scrollY > 0) {
-            setIsScrolled(true)
-          } else {
-            setIsScrolled(false)
-          }
-        }
-    
-        window.addEventListener('scroll', handleScroll)
-    
-        return () => {
-          window.removeEventListener('scroll', handleScroll)
-        }
-      }, [])
+    window.addEventListener("scroll", handleScroll);
 
-    return (
-    <header className={`${isScrolled && 'bg-[#141414]'}`}>
-        <div className="flex items-center space-x-2 md:space-x-10">
-            <img
-                src="https://i.postimg.cc/6p42F4np/Netflix-2015-logo.png"
-                width={100}
-                height={100}
-                className="cursor-pointer object-contain"
-            />
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-            <ul className="hidden space-x-4 md:flex">
-                <li className="headerLink">Home</li>
-                <li className="headerLink">TV Shows</li>
-                <li className="headerLink">Movies</li>
-                <li className="headerLink">New & Popular</li>
-                <li className="headerLink">My list</li>
-            </ul>
-        </div>
+  return (
+    <header className={`${isScrolled && "bg-[#141414]"}`}>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img
+          src="https://i.postimg.cc/6p42F4np/Netflix-2015-logo.png"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+      </div>
 
-        <div className="flex items-center space-x-4 text-sm font-light">
-            <SearchIcon className="hidden h-6 w-6 sm:inline" />
-            <p className="hidden lg:inline">Kids</p>
-            <BellIcon className="h-6 w-6" />
-            <Link href="/account"> 
-                <img
-                 onClick={logout}
-                 src="https://rb.gy/g1pwyx" 
-                 alt="" 
-                 className="cursor-pointer rounded" />
-            </Link>
-        </div>
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="hidden h-6 w-6 sm:inline" />
 
+        <Link href="/account">
+          <img
+            onClick={logout}
+            src="./assets/icons/log-out.svg"
+            alt="log out icon"
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </div>
     </header>
-    )
+  );
 }
 
-export default Header
+export default Header;
